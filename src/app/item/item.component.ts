a06a8d984f01e9ae64d95d2aebf1cab0e2a5a0bb
yongqin123 <yongqin12324@gmail.com>
2025-07-27 23:18:33 +0800

    update item and men page

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HttpService } from '../http_service';
 import { Item } from '../Item';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './item.component.html',
   styleUrl: './item.component.css'
 })
@@ -22,5 +23,20 @@ export class ItemComponent {
         })
           
         ;
+  }
+  minusQuantity() {
+    var quantity = document.getElementById("quantity") as HTMLButtonElement
+    if (parseInt(quantity.value) > 1) {
+      quantity.value = (parseInt(quantity.value) - 1).toString();
+    }
+    
+  }
+
+  addQuantity() {
+    var quantity = document.getElementById("quantity") as HTMLButtonElement
+    if (parseInt(quantity.value) < 10) {
+      quantity.value = (parseInt(quantity.value) + 1).toString();
+    }
+    
   }
 }
